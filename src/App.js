0c17e4bcb3147d197b23fcee100076b38221bce0
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2020-12-29 13:48:19 -0800

    Create a Title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import React from 'react';
+import Title from './components/Title'
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Title/>
         <input />
       </div>
     );
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = () => (
+    <div>
+        <h1>Welcome to Nutrition Application!</h1>
+        <h2>It will help you build your Nutrition better, easier and faster!</h2>
+    </div>
+);
+
+export default Title;
