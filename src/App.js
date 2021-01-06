a2978b9b9d2787e0d874a86cbdaa99b6cc899c8c
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-01-05 17:03:21 -0800

    Used hook useForm and created form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
 import React from 'react';
-import Title from './components/Title'
+import Title from './components/Title';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
-class App extends React.Component {
-  render() {
+function App() {
+  const {register, handleSubmit, errors} = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
     return (
       <div className="App">
         <Title/>
         <input />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" placeholder="Email" name="email" ref={register}/>
+          <input 
+            type="password" 
+            placeholder="Password" 
+            name="password" 
+            ref={register({ required: true, minLength: 8 })}
+          />
+          {errors.password && <p>Password is invalid</p>}
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
-  }
 }
 
 export default App;
